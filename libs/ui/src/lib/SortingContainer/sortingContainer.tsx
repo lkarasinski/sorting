b3350c20ad7923e9f3f56a654b1d6ed5ac4a82fc
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-27 17:32:52 +0200

    refactor: :recycle: Move algorithm select to SortingContainer

--- libs/ui/src/lib/SortingContainer/sortingContainer.tsx
+++ libs/ui/src/lib/SortingContainer/sortingContainer.tsx
@@ -1,27 +1,55 @@
 import * as React from 'react';
-import { BoxStateType } from '@sorting/types';
+import { BoxStateType, SortingAlgorithms } from '@sorting/types';
 import { SortingBox } from '../SortingBox';
 import { Container } from './sortingContainer.style';
+import _ from 'lodash';
 
 type Props = {
   visualizationData: {
     values: number[];
     states: BoxStateType[];
   };
+  animating: boolean;
+  selectedAlgorithm: string;
+  changeSelectedAlgorithm: (value: SortingAlgorithms) => void;
+  availableAlgorithms: SortingAlgorithms[];
 };
 
-export const SortingContainer = ({ visualizationData }: Props) => {
+export const SortingContainer = ({
+  visualizationData,
+  animating,
+  selectedAlgorithm,
+  changeSelectedAlgorithm,
+  availableAlgorithms,
+}: Props) => {
   return (
-    <Container css={{ gap: 100 / visualizationData.values.length }}>
-      {visualizationData.values.map((number, index) => {
-        return (
-          <SortingBox
-            state={visualizationData.states[index]}
-            key={`${number} - ${index}`}
-            value={number}
-          />
-        );
-      })}
-    </Container>
+    <>
+      <Container
+        css={{ gap: Math.floor(100 / visualizationData.values.length) }}
+      >
+        {visualizationData.values.map((number, index) => {
+          return (
+            <SortingBox
+              state={visualizationData.states[index]}
+              key={`${number} - ${index}`}
+              value={number}
+            />
+          );
+        })}
+      </Container>
+      <select
+        disabled={animating}
+        value={selectedAlgorithm}
+        onChange={(e) =>
+          changeSelectedAlgorithm(e.target.value as SortingAlgorithms)
+        }
+      >
+        {[selectedAlgorithm, ...availableAlgorithms].map((algorithm) => (
+          <option key={algorithm} value={algorithm}>
+            {_.capitalize(algorithm)} Sort
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
