b4a8b2cb7641d87f37480b386c880716aeb25c0b
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-30 18:11:30 +0200

    feat: :sparkles: Add useVisualizationData hook

---
+++ libs/hooks/src/lib/useVisualizationData/useVisualizationData.tsx
@@ -0,0 +1,67 @@
+import { generateNewArray } from '@sorting/functions';
+import {
+  SortingAlgorithms,
+  SortingAlgorithmsData,
+  VisualizationData,
+} from '@sorting/types';
+import { useDebounce, useMemoizedFn } from 'ahooks';
+import * as React from 'react';
+
+type UpdateData = (sort: SortingAlgorithms, data: VisualizationData) => void;
+type ResetData = () => void;
+
+export type UseVisualizationData = (
+  arrayLenght: number,
+  setState: React.Dispatch<
+    React.SetStateAction<'randomized' | 'sorting' | 'sorted'>
+  >
+) => [SortingAlgorithmsData, UpdateData, ResetData];
+
+export const useVisualizationData: UseVisualizationData = (
+  arrayLength,
+  setState
+) => {
+  const debouncedLength = useDebounce(arrayLength, { wait: 300 });
+
+  const [visualizationData, setVisualizationData] =
+    React.useState<SortingAlgorithmsData>(() => {
+      const newData = generateNewArray(debouncedLength);
+      return {
+        quick: JSON.parse(JSON.stringify(newData)),
+        bubble: JSON.parse(JSON.stringify(newData)),
+        insertion: JSON.parse(JSON.stringify(newData)),
+        selection: JSON.parse(JSON.stringify(newData)),
+        heap: JSON.parse(JSON.stringify(newData)),
+        gnome: JSON.parse(JSON.stringify(newData)),
+      };
+    });
+
+  const resetData: ResetData = useMemoizedFn(() => {
+    const newData = generateNewArray(debouncedLength);
+    setVisualizationData({
+      quick: JSON.parse(JSON.stringify(newData)),
+      bubble: JSON.parse(JSON.stringify(newData)),
+      insertion: JSON.parse(JSON.stringify(newData)),
+      selection: JSON.parse(JSON.stringify(newData)),
+      heap: JSON.parse(JSON.stringify(newData)),
+      gnome: JSON.parse(JSON.stringify(newData)),
+    });
+  });
+
+  React.useEffect(() => {
+    resetData();
+    setState('randomized');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedLength]);
+
+  const updateData: UpdateData = useMemoizedFn(
+    (sort: SortingAlgorithms, data: VisualizationData) => {
+      setVisualizationData({
+        ...visualizationData,
+        [sort]: data,
+      });
+    }
+  );
+
+  return [visualizationData, updateData, resetData];
+};
